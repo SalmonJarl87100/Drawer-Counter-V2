10edb00b4962b887dfa04c6e0d0fb96ea66ab77e
SalmonJarl87100 <93258398+SalmonJarl87100@users.noreply.github.com>
2022-07-25 13:23:41 -0400

    Comments
    Added comments to clarify functions and code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter.ttk import *
 
 
 class Window:
-    def __init__(self, parent):
+    def __init__(self, parent):  # this creates all necessary widgets and data for runtime
         self.root = parent
 
         self.money = {
@@ -19,18 +19,21 @@ class Window:
             '0.01': 0.01
         }
 
+        # creates the day cash label and entry widgets
         self.dayCashLbl = Label(self.root, text="Day cash: $")
 
         self.dayCashTxtVar = StringVar()
         self.dayCashEtry = Entry(self.root, width=6, textvariable=self.dayCashTxtVar)
         self.dayCashTxtVar.trace('w', lambda *args: numFilter(self.dayCashTxtVar, deci=True))
 
+        # creates the target drawer label and entry widgets
         self.targetLbl = Label(self.root, text="Target:      $")
 
         self.targetStrVar = StringVar()
         self.targetEtry = Entry(self.root, width=6, textvariable=self.targetStrVar)
         self.targetStrVar.trace('w', lambda *args: numFilter(self.targetStrVar, deci=True))
 
+        # this creates the bill amount label and entry widgets
         self.moneyFrame = Frame(self.root)
         self.moneyVars = []
         self.moneyEtries = []
@@ -39,12 +42,12 @@ class Window:
             self.moneyLbls.append(Label(self.moneyFrame, text=f"${bill}:"))
 
             self.moneyVars.append(StringVar())
-
-            self.moneyEtries.append(Entry(self.moneyFrame, width=4, textvariable=self.moneyVars[-1]))
-
             self.moneyVars[-1].trace('w', lambda *args: numFilter(self.moneyVars[-1]))
 
-    def setUp(self):
+            # TODO: fix bug that breaks entry filtering with bill entries
+            self.moneyEtries.append(Entry(self.moneyFrame, width=4, textvariable=self.moneyVars[-1]))
+
+    def setUp(self):  # this grids all startup widgets created by init
         self.dayCashLbl.grid(column=0, row=0, sticky='nw')
         self.dayCashEtry.grid(column=1, row=0, sticky='nw')
 
@@ -59,6 +62,13 @@ class Window:
 
 
 def numFilter(txtVar, deci=False, *args):
+    """
+    this can be set to a string var to filter out any non number character
+    :param txtVar: a tk string var that will be filtered
+    :param deci: if a decimal should be allowed
+    :param args: throwaway arg for extra arguments
+    :return: none
+    """
     currVal = splitStr(txtVar.get())
 
     if len(currVal) == 0:
@@ -78,10 +88,21 @@ def numFilter(txtVar, deci=False, *args):
 
 
 def splitStr(val):
+    """
+    this func splits a string into a list
+    :param val: this should be a string
+    :return: a list version of val
+    """
     return [char for char in val]
 
 
 def countChar(lis, char):
+    """
+    this counts how many of a specified char in a string or list
+    :param lis: string or list to be tested
+    :param char: the character to be counted
+    :return: number of characters in given list or string based on given char
+    """
     charAmount = 0
 
     for character in lis:
@@ -92,6 +113,11 @@ def countChar(lis, char):
 
 
 def valInt(instr):
+    """
+    this verifies that the given data is an integer
+    :param instr: given data to be tested
+    :return: true if int, false if value error
+    """
     try:
         int(instr)
         return True
